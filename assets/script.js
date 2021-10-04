7fc7ba3ac092c29cf75efe04c2ddbb2cb4a0b9e9
Preston <prestonconley5@gmail.com>
2021-10-04 09:47:58 -0500

    added folders for better organization

--- assets/script.js
+++
@@ -1,1 +0,0 @@
-console.log('kathy was here');
---
+++ assets/scripts/script.js
@@ -0,0 +1,10 @@
+console.log('kathy was here');
+
+//user fills out search field
+//call the api based on user input
+//brings you to results page
+//results page shows a few results recipes from the api
+//user can scroll through the results 
+//user selects results of thier choice
+//user is brought to recipe page 
+//recipe page is populated by JS with data for selected result
