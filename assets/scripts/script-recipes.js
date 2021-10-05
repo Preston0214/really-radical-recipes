5a9006b9646f1ecc3d347c623ef15abc9013a9ab
j-spencer11 <jackspencer0806@gmail.com>
2021-10-05 14:47:47 -0500

    added javascript for populating recipe page

--- assets/scripts/script-recipes.js
+++ assets/scripts/script-recipes.js
@@ -1,13 +1,34 @@
 var recipeId = localStorage.getItem('recipeId');
+var recipeImage = $('#recipe-image');
+var recipeTitle = $('#recipe-title');
+var recipeIngredients = $('#recipe-ingredients');
+var recipeLink = $('#recipe-link');
+var recipeSummary = $('#recipe-summary');
 
 function getFullInfo(){
-    fetch('https://api.spoonacular.com/recipes/'+recipeId+'/information?apiKey=e31330a3bd494aaa8323e1a4293b775d')
+    fetch('https://api.spoonacular.com/recipes/'+recipeId+'/information?apiKey=f6afd4ae0a8c4afc84874cd3960737aa')
     .then(function (res){
       if(res.ok){
         res.json().then(function(data){
           console.log(data)
+          populatePage(data)
         })
       }
     })
   }
   getFullInfo()
+
+  function populatePage(data){
+recipeImage.attr('src', data.image)
+recipeTitle.text(data.title)
+recipeSummary.html(data.summary)
+var ingredientsArray = data.extendedIngredients;
+for(var i=0; i<ingredientsArray.length; i++){
+    var listItem = $('<li>').text(ingredientsArray[i].name)
+    recipeIngredients.append(listItem)
+    console.log(listItem)
+}
+recipeLink.onclick = function () {
+    location.href = data.sourceUrl
+}
+  }
